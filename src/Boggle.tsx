4f33492716b315ce8b9d42e7bc58d550517b5e3c
Your Name <you@example.com>
2021-10-26 19:42:59 -0400

    Check if currently selected square is adjecent to already selected ones

--- src/Boggle.tsx
+++ src/Boggle.tsx
@@ -4,7 +4,7 @@ import { Square } from "./Square";
 
 export const Boggle: React.FC = () => {
     const [boardState, setBoardState] = useState<string[][]>()
-    const [selected, setSelected]=useState<number[][]>([]);
+    const [selected, setSelected] = useState<number[][]>([]);
     useEffect(() => {
         const board = [];
         const size = 5;
@@ -21,19 +21,33 @@ export const Boggle: React.FC = () => {
         setBoardState(board);
     }, []);
     console.log(selected);
+    function isAdjacent(adjacentTo: number[][], square: number[]) {
+        return adjacentTo.some(element => {
+            if (
+                (square[0] === element[0] || square[0] === element[0] + 1 || square[0] === element[0] - 1) &&
+                (square[1] === element[1] || square[1] === element[1] + 1 || square[1] === element[1] - 1)
+            ) {
+                return true;
+            }
+        })
+    }
     return <>
-    <p>Currently selected: {selected.map(index2D=>boardState && boardState[index2D[0]][ index2D[1]])}</p>
+        <p>Currently selected: {selected.map(index2D => boardState && boardState[index2D[0]][index2D[1]])}</p>
         {
             boardState && boardState.map((row, i) => {
                 return <div>{row.map((letter, j) => {
-                    return <Square letter={letter} onClick={()=>{
-                        const index=selected.findIndex(element=>element[0]===i && element[1]===j);
-                        if (index===-1) {
-                            setSelected([...selected, [i,j]])
-                        } else {
-                            setSelected([...selected.slice(0, index), ...selected.slice(index+1)])
+                    return <Square letter={letter} onClick={() => {
+                        if (selected.length===0) {
+                            setSelected([[i, j]]);
+                            return;
                         }
-                    }}/>
+                        const index = selected.findIndex(element => element[0] === i && element[1] === j);
+                        if (index === -1 && isAdjacent(selected, [i, j])) {
+                            setSelected([...selected, [i, j]])
+                        } else {
+                                setSelected([...selected.slice(0, index), ...selected.slice(index + 1)])
+                            }
+                        }} />
                 })}</div>
             }
             )
