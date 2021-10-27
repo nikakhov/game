7b53826a220fafd481b0e2d109eaebac6f8bdeea
Your Name <you@example.com>
2021-10-26 21:04:28 -0400

    Add points and word table

--- src/Boggle.tsx
+++ src/Boggle.tsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from "react";
 import { LetterDistribution } from "./LetterDistribution";
 import { Square } from "./Square";
 import { Dictionary } from "./Dictionary";
+import { Points } from "./Points";
 
 export const Boggle: React.FC = () => {
     const [boardState, setBoardState] = useState<string[][]>()
     const [selected, setSelected] = useState<number[][]>([]);
+    const [points, setPoints]=useState<number>(0);
+    const [submittedWords, setSubmittedWords]=useState<string[]>([]);
     useEffect(() => {
         const board = [];
         const size = 5;
@@ -33,10 +36,16 @@ export const Boggle: React.FC = () => {
         })
     }
 
-    const currentlySelected=selected.map(index2D => boardState && boardState[index2D[0]][index2D[1]]).join('');
+    const currentlySelectedWord=selected.map(index2D => boardState && boardState[index2D[0]][index2D[1]]).join('');
+    /*Could be improved with binary search tree because the list is sorted*/
+    const isInDictionary=Dictionary.indexOf(currentlySelectedWord)!==-1;
+    const alreadySubmitted=submittedWords.indexOf(currentlySelectedWord)!==-1;
     return <>
-        <p>Currently selected: {currentlySelected}</p>
-        <p>is {/*Could be improved with binary search tree because the list is sorted*/ Dictionary.indexOf(currentlySelected)===-1&& <b>not</b>} in dicionary</p>
+        <p>Currently selected: {currentlySelectedWord}</p>
+        <p>is {!isInDictionary && <b>not</b>} in dicionary</p>
+        <p>{alreadySubmitted && <b>has already been submitted</b>}</p>
+        <p>Points {points}</p>
+        <p>Submitted words:<br/> {submittedWords.map(word=><>{word}<br/></>)}</p>
         {
             boardState && boardState.map((row, i) => {
                 return <div>{row.map((letter, j) => {
@@ -56,5 +65,12 @@ export const Boggle: React.FC = () => {
             }
             )
         }
+        <button disabled={!isInDictionary || alreadySubmitted} onClick={()=>{
+            const length=currentlySelectedWord.length>8? '8' : currentlySelectedWord.length;
+            console.log("length", length);
+            setPoints(points+Points[length]);
+            setSubmittedWords([...submittedWords, currentlySelectedWord]);
+            setSelected([]);
+        }}> Submit </button>
     </>;
 }
